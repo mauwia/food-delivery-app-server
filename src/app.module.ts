d0d6716871051bad189b242fd4ef7f2865c0404d
mauwia <mauwia.atif@gmail.com>
2020-12-04 18:21:09 +0500

    initialize twilio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import {TwilioModule} from 'nestjs-twilio'
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
@@ -11,6 +12,10 @@ import { AuthModule } from "./auth/auth.module";
     MongooseModule.forRoot( `mongodb+srv://${process.env.MONGOOSE_PASSWORD}:nestonep@cluster0.arej3.mongodb.net/${process.env.MONGOOSE_DB_NAME}?retryWrites=true&w=majority`,{
       useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true
     }),
+    TwilioModule.forRoot({
+      accountSid:process.env.TWILIO_ACCOUNT_SID,
+      authToken:process.env.TWILIO_AUTH_TOKEN,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
