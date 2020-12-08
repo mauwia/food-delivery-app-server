16292bfa6cc74f624180578d25018d772b898551
mauwia <mauwia.atif@gmail.com>
2020-12-08 16:31:02 +0500

    updated:getUserRegesteredDevices

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,24 +1,26 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getModelToken } from "@nestjs/mongoose";
 import { AuthController } from "./auth.controller";
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from "bcryptjs";
 import { AuthService } from "./auth.service";
 import { Request } from "express";
 import { TwilioModule } from "nestjs-twilio";
-import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
-import {
-  Injectable,
-} from "@nestjs/common";
+import { InjectTwilio, TwilioClient } from "nestjs-twilio";
+import { Injectable } from "@nestjs/common";
+import { WalletService } from "../wallet/wallet.service";
 let event = {
   phoneNo: "123456789",
   passHash: "",
-  verified:false,
+  verified: false,
   location: [],
   imageUrl: "",
   username: "",
   mobileRegisteredId: "12345678",
-
 };
+class TwillioModel {
+  constructor() {}
+  static verify = jest.fn();
+}
 class eventModel {
   constructor() {}
   save = jest.fn().mockResolvedValue(event);
@@ -28,17 +30,17 @@ class eventModel {
   //         return null
   //     }
   // });
-  static findOne = jest.fn().mockImplementation(body=>{
-    if(body.phoneNo===event.phoneNo){
-        return event
+  static findOne = jest.fn().mockImplementation((body) => {
+    if (body.phoneNo === event.phoneNo) {
+      return event;
     }
-});
+  });
   static findOneAndUpdate = jest.fn().mockResolvedValue(event);
-  static create = jest.fn().mockImplementation(body=>{
-    event.phoneNo="12345"
-    event.passHash='12o2302'
-    event.mobileRegisteredId="body.mobileRegisteredId"
-    return event 
+  static create = jest.fn().mockImplementation((body) => {
+    event.phoneNo = "12345";
+    event.passHash = "12o2302";
+    event.mobileRegisteredId = "body.mobileRegisteredId";
+    return event;
   });
 
   static deleteOne = jest.fn().mockResolvedValue(true);
@@ -49,30 +51,36 @@ class InjectableService {
 }
 describe("AppController", () => {
   let authController: AuthController;
+  let verify: jest.Mock;
   let bcryptCompareSync: jest.Mock;
   beforeEach(async () => {
     bcryptCompareSync = jest.fn().mockReturnValue(true);
+    verify = jest.fn().mockReturnValue("432571");
+
     (bcrypt.compareSync as jest.Mock) = bcryptCompareSync;
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
-        AuthService,
+        AuthService,WalletService,
         {
           provide: getModelToken("Auth"),
           useValue: eventModel,
-        },InjectableService,
-        
+        },
+        {
+          provide: InjectableService,
+          useValue: TwillioModel,
+        },
       ],
-      imports:[TwilioModule.forRoot({
-        accountSid:process.env.TWILIO_ACCOUNT_SID,
-          authToken:process.env.TWILIO_AUTH_TOKEN,
-      })]
+      imports: [
+        TwilioModule.forRoot({
+          accountSid: process.env.TWILIO_ACCOUNT_SID,
+          authToken: process.env.TWILIO_AUTH_TOKEN,
+        }),
+      ],
     }).compile();
     authController = app.get<AuthController>(AuthController);
-
   });
-  test("FoodLoverSignUp", async() => {
-
+  test("FoodLoverSignUp", async () => {
     // let req = {
     //   body: {
     //     phoneNo: "123456789",
@@ -89,64 +97,61 @@ describe("AppController", () => {
     //     username: '',
     //     mobileRegisteredId: 'body.mobileRegisteredId'
     //   })
-  
   });
-  test("FoodLoverSignIn",async()=>{
-
-      let req = {
-        body: {
-          phoneNo: "123456789",
-          password: "string",
-        },
-      } as Request;
-      let response=await  authController.login(req)
-      expect(response.user).toStrictEqual({
-        phoneNo: '123456789',
-        passHash: '',
-        verified:false,
-        location: [],
-        imageUrl: '',
-        username: '',
-        mobileRegisteredId: '12345678'
-      })
-     
-  })
-  test('getLoverInfo',async ()=>{
-      let req={
-        user: {
-          phoneNo: "123456789"
-        }
-      } as unknown as Request
-      let response=await authController.LoverInfo(req)
-      expect(response.user).toStrictEqual({
-        phoneNo: '123456789',
-        passHash: '',
-        verified: false,
-        location: [],
-        imageUrl: '',
-        username: '',
-        mobileRegisteredId: '12345678'
-      }
-)
-  })
-
-  test('getUserRegisteredDevice',async ()=>{
-    let req={
-      user: {
-        phoneNo: "123456789"
-      }
-    } as unknown as Request
-    let response=await authController.GetUserRegisteredDevice(req)
-   expect(response.mobileRegisteredId).toBe("12345678")
-  })
-  test('authenticateOTP',()=>{
-    let req={
-      user: {
-        phoneNo: "123456789"
+  test("FoodLoverSignIn", async () => {
+    let req = {
+      body: {
+        phoneNo: "123456789",
+        password: "string",
       },
-      body:{
-        
-      }
-    } as unknown as Request
-  })
+    } as Request;
+    let response = await authController.login(req);
+    expect(response.user).toStrictEqual({
+      phoneNo: "123456789",
+      passHash: "",
+      verified: false,
+      location: [],
+      imageUrl: "",
+      username: "",
+      mobileRegisteredId: "12345678",
+    });
+  });
+  test("getLoverInfo", async () => {
+    let req = ({
+      user: {
+        phoneNo: "123456789",
+      },
+    } as unknown) as Request;
+    let response = await authController.LoverInfo(req);
+    expect(response.user).toStrictEqual({
+      phoneNo: "123456789",
+      passHash: "",
+      verified: false,
+      location: [],
+      imageUrl: "",
+      username: "",
+      mobileRegisteredId: "12345678",
+    });
+  });
+
+  test("getUserRegisteredDevice", async () => {
+    let req = ({
+      user: {
+        phoneNo: "123456789",
+      },
+    } as unknown) as Request;
+    let response = await authController.GetUserRegisteredDevice(req);
+    expect(response.mobileRegisteredId).toBe("12345678");
+  });
+  test("authenticateOTP", () => {
+    let req = ({
+      user: {
+        phoneNo: "123456789",
+      },
+      body: {
+        otp: "234563",
+      },
+      // const
+    } as unknown) as Request;
+  });
 });
--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,21 +1,19 @@
-import { Body, Controller,Get, Req } from '@nestjs/common';
-import { Request } from 'express';
-import { string } from 'is_js';
+import { Controller, Get, Req } from "@nestjs/common";
+import { Request } from "express";
 
-import { WalletService } from './wallet.service';
+import { WalletService } from "./wallet.service";
 
-@Controller('wallet')
-
+@Controller("wallet")
 export class WalletController {
-    constructor(private readonly walletService: WalletService) {}
-    @Get("/create")
-    async CreateWallet(){
-       let response=await this.walletService.createWallet()
-       return response
-    }
-    @Get("/getBalance")
-    async GetBalance(@Req() request:Request){
-       let response=await this.walletService.getBalance(request.body.address)
-       return response
-    }
+  constructor(private readonly walletService: WalletService) {}
+  @Get("/create")
+  async CreateWallet() {
+    let response = await this.walletService.createWallet();
+    return response;
+  }
+  @Get("/getBalance")
+  async GetBalance(@Req() request: Request) {
+    let response = await this.walletService.getBalance(request.body.address);
+    return response;
+  }
 }
