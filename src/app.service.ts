db14414825b20489cbc0738fbb33a1fdc3dbfe59
mauwia <mauwia.atif@gmail.com>
2021-01-09 18:21:54 +0500

    addMenu addMenuItem

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { pad } from './utils';
 
 @Injectable()
 export class AppService {
+  uniqueNumber='19000000'
   getHello(): string {
     return 'Hello World!';
+  }
+  getUniqueNumber(){
+    let incrementOrder= +this.uniqueNumber+1
+    // console.log(incrementOrder)
+    this.uniqueNumber= pad(incrementOrder,this.uniqueNumber.length)
+    return this.uniqueNumber
   }
 }
