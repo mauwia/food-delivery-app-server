0484d79e7a1ea382c38ea11ed22de1cd4cd2f271
mauwia <mauwia.atif@gmail.com>
2021-01-09 18:25:35 +0500

    deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,6 +11,6 @@ export class AppService {
     let incrementOrder= +this.uniqueNumber+1
     // console.log(incrementOrder)
     this.uniqueNumber= pad(incrementOrder,this.uniqueNumber.length)
-    return this.uniqueNumber
+    return {uniqueNumber:this.uniqueNumber}
   }
 }
