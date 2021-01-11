e4ddfc21fbc205eb290061397fff024a24131636
mauwia <mauwia.atif@gmail.com>
2021-01-12 01:12:06 +0500

    deploy heroku

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { pad } from './utils';
 
 @Injectable()
 export class AppService {
-  uniqueNumber='19000050'
+  uniqueNumber='20000000'
   getHello(): string {
     return 'Hello World!';
   }
