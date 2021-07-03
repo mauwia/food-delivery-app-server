dd6f206ae78b28d236d9fdb80835206f2487a9b3
mauwia <mauwia.atif@gmail.com>
2021-07-04 00:11:40 +0500

    fix in order

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -64,7 +64,7 @@ export class NotificationService {
           },
           {
             path: "orderId",
-            select: "orderId orderedFood",
+            select: "orderId orderedFood reason",
           },
         ]);
       return { notifications };
