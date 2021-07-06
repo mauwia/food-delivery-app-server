49af8ff665605f431433c67d1ff9ee2a8c73f806
Mary Mazi <mazi.mary.o@gmail.com>
2021-07-06 05:55:44 -0500

    feat (admin module): - add endpoint to get specified food creator

--- src/admin/food-creators/food-creators.controller.ts
+++ src/admin/food-creators/food-creators.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { ObjectId } from 'mongoose';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { QueryParams } from './interfaces';
 import { FoodCreatorsService } from '../food-creators/food-creators.service';
+import { FoodCreator } from '../../food-creator/food-creator.model';
 
 @Controller()
 export class FoodCreatorsController {
   constructor(private readonly adminFoodCreatorsService: FoodCreatorsService) {}
 
   @Get()
-  async getAllLovers (@Query() queryParams) {
+  async getAllCreators (@Query() queryParams: QueryParams): Promise<any> {
     return await this.adminFoodCreatorsService.getAllCreators(queryParams);
+  }
+
+  @Get('/:id')
+  async getCreator (@Param('id') id: ObjectId): Promise<FoodCreator> {
+    return await this.adminFoodCreatorsService.getCreator(id);
   }
 }
--- src/admin/food-creators/food-creators.service.ts
+++ src/admin/food-creators/food-creators.service.ts
@@ -1,4 +1,4 @@
-import { PaginateModel } from 'mongoose';
+import { PaginateModel, ObjectId } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FoodCreator } from "../../food-creator/food-creator.model";
@@ -20,14 +20,21 @@ export class FoodCreatorsService {
 
     if (queryParams.search) {
       query = {
-        $or: [
-          { businessName: new RegExp(queryParams.search) },
-          { username: new RegExp(queryParams.search) },
+        $or: [          
+          { email: new RegExp(queryParams.search, 'i') },
+          { username: new RegExp(queryParams.search, 'i') },
+          { businessName: new RegExp(queryParams.search, 'i') },
+          { 'location.address': new RegExp(queryParams.search, 'i') },
         ],
       };
     }
 
     const result = await this.foodCreatorModel.paginate(query, options);
     return getPaginatedResult(result);
+  }
+
+  async getCreator(id: ObjectId): Promise<FoodCreator> {
+    const result = await this.foodCreatorModel.findById(id);
+    return result;
   }
 }
