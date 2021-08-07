41ab5a98296a5ab2db01587b6e769b1242ad28f8
Mary Mazi <mazi.mary.o@gmail.com>
2021-08-07 15:53:27 -0500

    admin verification update - update verification status to Ongoing when switching around stages

--- src/admin/food-creators/food-creators.service.ts
+++ src/admin/food-creators/food-creators.service.ts
@@ -84,6 +84,8 @@ export class FoodCreatorsService {
   async updateVerificationStage(id, newStage) {
     if (newStage === 'Account Activated') {
       await this.updateVerificationStatus(id, 'Completed');
+    } else {
+      await this.updateVerificationStatus(id, 'Ongoing');
     }
     const updatedFC = await this.foodCreatorModel.findOneAndUpdate(
       { _id: id },
