b22df0b049eeea057cf922b95eb273e79484ce04
Mary Mazi <mazi.mary.o@gmail.com>
2021-08-12 10:17:53 -0500

    admin notification: - send notification email to admin on new FC signup

--- src/food-creator/food-creator.service.ts
+++ src/food-creator/food-creator.service.ts
@@ -10,6 +10,7 @@ import { Testers } from "src/profile/profile.model";
 import { InjectTwilio, TwilioClient } from "nestjs-twilio";
 import { AdminGateway } from "src/admin/admin.gateway";
 import { AdminNotificationService } from "src/admin/admin-notification/admin-notification.service";
+import { sendEmail as sendAdminNotificationEmail } from '../admin/shared/emailNotification';
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
@@ -114,6 +115,18 @@ export class FoodCreatorService {
           img: user?.imageUrl,
         });
         this.adminGateway.handleFCSignup({ notification, user });
+        process.env.ADMIN_EMAILS.split(" ").forEach(adminEmail => {
+          sendAdminNotificationEmail({
+            sender: {
+              name: 'Noshify',
+              email: process.env.SENDER_EMAIL,
+            },
+            recepient: adminEmail,
+            subject: 'New FC Signup',
+            phone: `+${user.countryCode}${user.phoneNo}`,
+            profileUrl: `${process.env.ADMIN_PORTAL_ROOT_URL}/admin/creators/${user._id}`
+          });
+        });
 
         const token = jwt.sign(
           { phoneNo: req.phoneNo },
